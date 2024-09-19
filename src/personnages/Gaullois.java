897abfacbf26cf95c1aaeb190a535d270b778bc6
Ocerti <158782700+Ocerti@users.noreply.github.com>
2024-09-19 11:33:51 +0200

    Initialisation des classes
    Initialisation des classes :
    -gaullois
    -romain
    -druide
    -village
    -chef

---
+++ src/personnages/Gaullois.java
@@ -0,0 +1,39 @@
+package personnages;
+
+public class Gaullois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaullois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	private String prendreParole() { 
+		return "Le gaulois " + nom + " : ";
+
+	}
+	
+	public void frapper(Romain romain) { 
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+	
+	@Override
+	public String toString() {
+		return "Gaullois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+		//TODO créer un main permettant de tester la classe Gaulois
+	}
+}
