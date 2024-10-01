03a7e30c56443aadc9ebeb0b47d7e410479fdeb5
Ocerti <158782700+Ocerti@users.noreply.github.com>
2024-10-01 09:45:22 +0200

    sur sswich case petit prblm
    attention 2024 idl et on fais un switch sur le nombre d'equipement donc soit on en a 2 donc r soit 1 et bah on regarde c quoi soit 0 et la c ez

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,16 +3,39 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 		assert (force >= 0);
+		Equipement[] equipement = new Equipement[Equipement.BOUCLIER.tostring() , Equipement.CASQUE.tostring()];
 	}
 
 	public String getNom() {
 		return nom;
 	}
+	
+	public void sEquiper() {
+		switch (nbEquipement) {
+		case 2 :
+			print("full");
+			break;
+		case equipement[1] == "casque":
+			print("deja equip�");
+			break;
+		default:
+			equipement[nbEquipement]= "casque"
+			
+		
+		}
+	}
+	
+	
+	
+	
+	
 
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
@@ -43,6 +66,10 @@ public class Romain {
 		Romain Ravus = new Romain("Ravus",6);
 		Ravus.parler(Ravus.prendreParole());;
 		Ravus.recevoirCoup(1);
+		System.out.println(Equipement.BOUCLIER);
+		System.out.println(Equipement.CASQUE);
+
+		
 	}
 }
 
